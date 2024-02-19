2a9b96c4d1db812a38a471dbd8b810edc9bd633b
shaker-ullah <shakeru449@gmail.com>
2024-02-19 21:38:52 +0600

    seat booking section added

--- script/uitility.js
+++ script/uitility.js
@@ -1,6 +1,8 @@
 const allSeats = document.getElementsByClassName('seat-btn');
 let count = 0;
 let booking = 24;
+let perSeatPrice = 550;
+let seatClass = 'Economy';
 for (const btn of allSeats) {
     // console.log(btn)
     btn.addEventListener('click', (e) => {
@@ -10,7 +12,22 @@ for (const btn of allSeats) {
 
         const seatName = e.target.innerText;
         // console.log(seatName)
-        console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes)
+
+        const bookingSeat = document.getElementById('new-ticket');
+        const li = document.createElement('li')
+        const p = document.createElement('p');
+        p.innerText = seatName;
+        const p2 = document.createElement('p');
+        p2.innerText = seatClass;
+        const p3 = document.createElement('p');
+        p3.innerText = perSeatPrice;
+        li.appendChild(p)
+        li.appendChild(p2)
+        li.appendChild(p3)
+        bookingSeat.appendChild(li)
+
+        // const perSeatPrice = (e.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[3].childNodes[3].childNodes[1].childNodes[1].innerText)
+        // console.log(perSeatPrice)
 
 
         setInnerText('count-seats', count)
