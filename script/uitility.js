3056655b127c6cb5618c8d2963498bfa1193630b
shaker ullah <shakeru449@gmail.comA>
2024-02-19 19:44:05 +0600

    shaker add@

--- script/uitility.js
+++ script/uitility.js
@@ -1,9 +1,26 @@
-const allSeats = document.getElementsByClassName('btn');
+const allSeats = document.getElementsByClassName('seat-btn');
 let count = 0;
+let booking = 24;
 for (const btn of allSeats) {
     // console.log(btn)
     btn.addEventListener('click', (e) => {
+        btn.style.backgroundColor = 'green'
         count = count + 1
-       document.getElementById('count-seats').innerText = count;
+        booking = booking - 1;
+
+        const seatName = e.target.innerText;
+        // console.log(seatName)
+        console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes)
+
+
+        setInnerText('count-seats', count)
+        setInnerText('booking-seats', booking)
     })
+}
+
+// innerText function
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+
+
 }
