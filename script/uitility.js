8ab737e0a2086d0d187642a6a42a5886f38f76d0
shaker ullah <shakeru449@gmail.comA>
2024-02-21 13:36:46 +0600

    limitet seats added

--- script/uitility.js
+++ script/uitility.js
@@ -4,9 +4,13 @@ for (const btn of allBtn) {
     // console.log(btn)
 
     let seatClass = 'Ac-class'
-
+   
     btn.addEventListener('click', function (event) {
-
+        const perPersonBuy = getInnerTextValue('count-seats')
+        if (perPersonBuy + 1 > 4) {
+            alert('Your limit is close')
+            return;
+        }
         const seatPrice = getInnerTextValue('per-seat')
         // console.log(seatPrice)
         const seatName = event.target.innerText;
@@ -15,9 +19,18 @@ for (const btn of allBtn) {
         document.getElementById('count-seats').innerText = seat + 1
         const haveSeat = getInnerTextValue('booking-seats')
         document.getElementById('booking-seats').innerText = haveSeat - 1;
-
+        
         const selectedDiv = document.getElementById('new-ticket');
         // create New Element
+
+
+       
+       
+
+
+
+
+
         const div = document.createElement('div')
         div.classList.add('flex')
         div.classList.add('justify-between')
@@ -34,21 +47,46 @@ for (const btn of allBtn) {
         div.appendChild(p3)
         selectedDiv.appendChild(div)
 
-
+        //    function call 
         totalCost(seatPrice)
+        updateGrandTotal()
     })
 }
 
 
+// function 
+function updateGrandTotal(coupon) {
+    const totalPrice = getInnerTextValue('total-price')
+    // use  coupon
+    if (coupon) {
+        const couponCode = document.getElementById("apply-coupon").value;
+
+
+        if ((couponCode == 'NEW15') || (couponCode == 'couple 20')) {
+            const discount = totalPrice * 0.1;
+            // console.log(discount)
+            document.getElementById('grand-total').innerText = totalPrice - discount
+        }
+
+        else {
+            alert('please a use valid coupon code')
+        }
+    }
+    else {
+        const totalPrice = getInnerTextValue('total-price')
+        // console.log(totalPrice, grandTotal)
+        document.getElementById('grand-total').innerText = totalPrice;
+    }
+
+
+}
 
 function totalCost(price) {
     const totalPrice = getInnerTextValue('total-price')
     // console.log(totalPrice)
-     document.getElementById('total-price').innerText = totalPrice + price;
-    
+    document.getElementById('total-price').innerText = totalPrice + price;
+
 }
-
-
 
 function getInnerTextValue(id) {
     const innerTextValue = document.getElementById(id).innerText;
